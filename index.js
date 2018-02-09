4e167581b648897448b52f0850cf80ac73530938
Agnieszka Sierbista <agnieszka.sierbista@gmail.com>
2018-02-09 15:07:44 +0100

    Add exercises

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 'use strict'
+
+// this is how .reduce works
+const strings = ["asddfdf", "dfdferetrt", "orabdfigr", "fff"];
+
+const concatenatedStrings = strings.reduce(concatenate, "");
+
+function concatenate(acc, string) {
+  return acc + string;
+};
+
+console.log(concatenatedStrings);
+
+const numbers = [3, 5, 6, 2, 7, 32, 78, 11, 34, 1, 34, 65, 33];
+
+const summedNumbers = numbers.reduce(sumUp, 0);
+
+function sumUp(acc, number) {
+  return acc + number;
+};
+
+console.log(summedNumbers);
+
+const object1 = numbers.reduce(passToObject, {});
+
+function passToObject(acc, number) {
+  return { ...acc, [number]: "dupa " + number };
+};
+
+console.log(object1);
+
